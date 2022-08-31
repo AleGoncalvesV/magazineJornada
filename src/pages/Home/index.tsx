975b310a61fb8360828d59feb71a58c46e9fe2a9
Alain <alaindelonm@gmail.com>
2022-08-31 12:31:53 -0300

    feat: tela de login

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,6 @@
-import { Button } from "@mui/material";
-
 function Home() {
   return (
-    <>
-      <div>Home</div>
-      <Button color="success" style={{ backgroundColor: "black" }}>
-        Click
-      </Button>
-    </>
+    <h1>Home</h1>
   );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import Login from "../pages/Login";
 
 export const AppRoutes = () => {
 
   return (
     <Routes>
       <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<Login />} />
 
       <Route path="*" element={<Navigate to="/home" />} />
     </Routes>
